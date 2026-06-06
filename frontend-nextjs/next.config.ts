38864e02e1c2f5ca16cf9b09a6e10a1d09a1cb14
ChiKhang250224 <22130112@st.hcmuaf.edu.vn>
2026-06-06 14:55:15 +0700

    fix: disable image optimization to fix 402 payment required error

--- frontend-nextjs/next.config.ts
+++ frontend-nextjs/next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "placehold.co" },
       { protocol: "https", hostname: "images.unsplash.com" },
